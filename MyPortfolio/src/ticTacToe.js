c12c7411e07e42006f5fabfb296df62a4135acd8
kemora08 <54960171+kemora08@users.noreply.github.com>
2019-12-06 16:57:30 -0600

    Update ticTacToe.js

--- MyPortfolio/src/ticTacToe.js
+++ MyPortfolio/src/ticTacToe.js
@@ -1,1 +1,31 @@
+$(document).ready(function() {
+var turns = ["#","#","#","#","#","#","+","#"];
+var computerTurn = "";
+var turn = "";
+var gameon = false;
+var count = 0;
 
+var startTurn = prompt("Choose Your Move", "Type X or O").toUpperCase();
+switch (startTurn) {
+   case "X":
+      computerTurn = "0";
+      turn = "X";
+      $("message").html("Player" + turn +"gets to start!");
+      break;
+   case "0":
+       computerTurn = "X";
+       turn = "O";
+       $("#message").html("player" + turn + "gets to start!");
+   break;
+   case null:
+       alert("Sorry. Please type X or O");
+       window.location.reload(true);
+   break;
+   default:
+       alert("Sorry. Please type X or O");
+       window.location.reload(true);
+   break;
+   default:
+}
+    
+});
